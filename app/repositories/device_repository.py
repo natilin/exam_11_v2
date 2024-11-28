f7e0f2d832a98d30dd2a6c1c0199e975ff306057
Nati Lintzer <nlinzer@gmail.com>
2024-11-28 17:06:49 +0200

    add get_strong_connection endpoint

--- app/repositories/device_repository.py
+++ app/repositories/device_repository.py
@@ -1,10 +1,37 @@
+from returns.maybe import Maybe
+
 from app.db.database import driver
 
 
-def get_device_connected_path():
+def get_device_bluetooth_connected_path() -> Maybe:
     with driver.session() as session:
         query = """
-        match path=((d:Device) -[:CONNECTED{method:"Bluetooth"}]->(:Device))
-        return path
+        MATCH (start:Device)
+        MATCH (end:Device)
+        WHERE start <> end
+        MATCH path = shortestPath((start)-[:CONNECTED*]->(end))
+        WHERE ALL(r IN relationships(path) WHERE r.method = 'Bluetooth')
+        WITH path, length(path) as pathLength
+        ORDER BY pathLength DESC
+        LIMIT 1
+        RETURN path
         """
-        return session.run(query).data()
+        return Maybe.from_optional(session.run(query).data())
+
+
+def get_strong_signal_connected_path(min_strength=-30) -> Maybe:
+    with driver.session() as session:
+        query = """
+        MATCH (start:Device)
+        MATCH (end:Device)
+        WHERE start <> end
+        MATCH path = shortestPath((start)-[:CONNECTED*]->(end))
+        WHERE ALL(r IN relationships(path) WHERE r.signal_strength_dbm >= $min_strength)
+        WITH path, length(path) as pathLength
+        ORDER BY pathLength DESC
+        LIMIT 1
+        RETURN path
+        """
+        params = {"min_strength": min_strength}
+        return Maybe.from_optional(session.run(query, params).data())
+
--- app/routes/device_routes.py
+++ app/routes/device_routes.py
@@ -1,6 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from app.repositories.device_repository import get_device_bluetooth_connected_path, get_strong_signal_connected_path
 
 device_blueprint = Blueprint("device", __name__)
 
-@device_blueprint.route("/connection", methods=["GET"])
-def get_all_connection():
+@device_blueprint.route("/bluetooth-connection", methods=["GET"])
+def get_longest_connection():
+    res = get_device_bluetooth_connected_path()
+    return jsonify(res.value_or({})), 200
+
+
+@device_blueprint.route("/strong-connection", methods=["GET"])
+def get_strong_connection():
+    res = get_strong_signal_connected_path()
+    return jsonify(res.value_or({})), 200
